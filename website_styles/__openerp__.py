9d1b7cfb3f563686acef469669d89c027c1659d6
D.H. Bahr <dhbahr@gmail.com>
2016-01-06 12:11:45 -0500

    Version 1.0

---
+++ website_styles/__openerp__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Website Global Styles',
+    'description': "Modify the website look and feel.",
+    'version': '1.0',
+    'author': 'D.H. Bahr',
+
+    'data': [
+        'views.xml',
+    ],
+
+    'category': 'Website',
+    'depends': ['website'],
+    'application': False,
+}
